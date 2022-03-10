1630e43c9c0c494097c0e6aa58edba136f75c7aa
haosappro <haop7078@gmail.com>
2022-03-10 19:47:23 +0700

    add new project

---
+++ CNPMNC/CNPMNC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNPMNC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            int ketqua;
+
+            ketqua = a + b;
+            Console.WriteLine("dap an la :",ketqua);
+        }
+    }
+}
